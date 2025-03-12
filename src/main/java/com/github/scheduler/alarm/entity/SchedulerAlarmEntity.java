0349843abbf3df55e2a557e4e9f58f20f91130fa
junhui4005 <100547902+junhui4005@users.noreply.github.com>
2025-03-12 11:10:48 +0900

    알람 & 마이페이지 초기 구현

---
+++ src/main/java/com/github/scheduler/alarm/entity/SchedulerAlarmEntity.java
@@ -0,0 +1,45 @@
+package com.github.scheduler.alarm.entity;
+import com.github.scheduler.auth.entity.UserEntity;
+import com.github.scheduler.calendar.entity.CalendarEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "schedule_alarm")
+public class SchedulerAlarmEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "calendar_id", nullable = false)
+    private CalendarEntity calendar;
+
+    @ManyToOne
+    @JoinColumn(name = "schedule_id", nullable = false)
+    private ScheduleEntity schedule;
+
+    @Column(name = "is_checked", nullable = false)
+    private boolean isChecked = false;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @PreUpdate
+    public void setUpdatedAt() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/resources/SQL/scheduler_alarm.sql
@@ -0,0 +1,9 @@
+CREATE TABLE scheduler_alarm (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    calendar_id BIGINT NOT NULL,
+    scheduler_id BIGINT NOT NULL,
+    is_checked BOOLEAN DEFAULT FALSE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
