5a1ab2099ca7e3ff40dc688d50fef632521b32d2
Jiyoung <jiyoung0910.kim@gmail.com>
2025-03-14 02:14:03 +0900

    [Feature] 채팅방 생성 API 구현

--- src/main/java/com/github/scheduler/chat/handler/ChatEventHandler.java
+++ src/main/java/com/github/scheduler/chat/handler/ChatEventHandler.java
@@ -4,10 +4,20 @@ import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.OnEvent;
 import com.github.scheduler.chat.dto.ChatRoomCreate;
+import com.github.scheduler.chat.entity.ChatRoom;
 import com.github.scheduler.chat.service.ChatService;
+import com.github.scheduler.global.config.auth.custom.CustomUserDetails;
+import com.github.scheduler.global.dto.ApiResponse;
+import com.github.scheduler.global.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Component;
 
+@Tag(name = "Socket.io Chat", description = "Socket.io 기반 채팅 API")
 @Component
 @RequiredArgsConstructor
 public class ChatEventHandler {
@@ -19,14 +29,28 @@ public class ChatEventHandler {
     // 클라이언트 연결 종료 시 실행
 
     // 채팅방 생성
+    @Operation(summary = "채팅방 생성" , description = "캘린더 생성 시 채팅방도 같이 생성")
     @OnEvent("createRoom")
-    public void onCreateRoom(SocketIOClient client, ChatRoomCreate roomCreate ) {
+    public ResponseEntity<ApiResponse<ChatRoom>> onCreateRoom(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                              SocketIOClient client, ChatRoomCreate roomCreate ) {
         client.joinRoom(roomCreate.getName());
+        client.sendEvent("createRoom", roomCreate.getName());
+        checkUser(customUserDetails);
+        return ResponseEntity.ok(chatService.createRoom(roomCreate));
     }
     // 채팅방 입장
+    @Operation(summary = "채팅방 입장", description = "채팅방에 참여")
+    @OnEvent("joinRoom")
+
 
     // 메시지 전송
 
     // 메시지 읽음
 
+    private void checkUser(CustomUserDetails customUserDetails) {
+        if (customUserDetails == null) {
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(ApiResponse.fail(ErrorCode.NOT_AUTHORIZED_USER));
+        }
+    }
 }
--- src/main/java/com/github/scheduler/chat/service/ChatService.java
+++ src/main/java/com/github/scheduler/chat/service/ChatService.java
@@ -1,5 +1,13 @@
 package com.github.scheduler.chat.service;
 
+import com.github.scheduler.auth.service.UserService;
+import com.github.scheduler.calendar.entity.CalendarEntity;
+import com.github.scheduler.chat.dto.ChatRoomCreate;
+import com.github.scheduler.chat.entity.ChatRoom;
+import com.github.scheduler.chat.repository.ChatMessageRepository;
+import com.github.scheduler.chat.repository.ChatRoomRepository;
+import com.github.scheduler.chat.repository.ChatRoomUserRepository;
+import com.github.scheduler.global.dto.ApiResponse;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -7,6 +15,20 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class ChatService {
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatRoomUserRepository chatRoomUserRepository;
+    private final ChatMessageRepository chatMessageRepository;
+    //private final CalendarRepository
+    public ApiResponse<ChatRoom> createRoom(ChatRoomCreate roomCreate) {
+        // 채팅방 생성
+        // get calendar entity
+        //CalendarEntity calendar
+        ChatRoom chatRoom = ChatRoom.builder()
+                .name(roomCreate.getName())
+                //.calendar(roomCreate.getCalendarId())
+                .build();
+        return ApiResponse.success(chatRoomRepository.save(chatRoom));
+    }
 
     // TODO : 읽음 처리 (동시성 처리 필요)
     // sol.1 : pessimistic lock 데이터를 조회할 때 락을 걸어 순차적으로 처리
