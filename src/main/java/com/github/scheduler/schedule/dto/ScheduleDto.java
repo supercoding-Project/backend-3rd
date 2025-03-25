71fdf32265491cec8640b2a4d366f46a1bf4c560
Lee hana <hn110178@naver.com>
2025-03-25 18:21:22 +0900

    프론트 요구 사항 적용

--- src/main/java/com/github/scheduler/schedule/dto/ScheduleDto.java
+++ src/main/java/com/github/scheduler/schedule/dto/ScheduleDto.java
@@ -16,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 public class ScheduleDto {
     private Long scheduleId; // 일정 Id
+    private String calendarId;
     private Long createUserId; // 팀 공유 캘린더에서 일정을 등록한 사용자
     private String title; // 일정 제목
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -25,7 +26,6 @@ public class ScheduleDto {
     private RepeatScheduleDto repeatSchedule; //일정 반복 설정
     private String location; // 일정 장소
     private String memo; //일정 설명
-    private String calendarId;
     private String status; //일정 상태
     private List<Long> sharedUserIds;  // 공유 캘린더인 경우 소속 유저 목록
 }
